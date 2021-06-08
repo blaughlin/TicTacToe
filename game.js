f301e3c31371a2ee78733f96f24ef05e265acc3c
Bernard Laughlin <bernardlaughlin@gmail.com>
2021-06-08 11:36:21 -0800

    imporved game board

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-const elem = document.getElementById('myCanvas')
+const canvas = document.getElementById('myCanvas')
 
 function collides(rects, x, y){
     let isCollision = false;
@@ -13,22 +13,28 @@ function collides(rects, x, y){
 }
 
 // check if context exists
-if (elem && elem.getContext){
-    const rects=[{x:0, y:0, width:50, height:50},
-                 {x:0, y:75, width:50, height:50}]
-    const context = elem.getContext('2d')
+if (canvas && canvas.getContext){
+    const rects=[{x:0, y:0, width:50, height:50}, {x:60, y:0, width:50, height:50}, {x:120, y:0, width:50, height:50},
+                 {x:0, y:60, width:50, height:50}, {x:60, y:60, width:50, height:50}, {x:120, y:60, width:50, height:50},
+                 {x:0, y:120, width:50, height:50}, {x:60, y:120, width:50, height:50}, {x:120, y:120, width:50, height:50}]
+    const context = canvas.getContext('2d')
     if (context){
         for(let i=0; i < rects.length; i++){
+            context.fillStyle = 'white'
             context.fillRect(rects[i].x, rects[i].y, rects[i].width, rects[i].height)
         } 
+        context.fillStyle = 'black'
+        context.fillRect(0, 50, 170, 10)
+        context.fillRect(0, 110, 170, 10)
+        context.fillRect(50, 0, 10, 170)
+        context.fillRect(110, 0, 10, 170)
     }
 
     // listener
-    elem.addEventListener('click', function(e) {
-        console.log('click ' + e.offsetX + '/' + e.offsetY)
+    canvas.addEventListener('click', function(e) {
         const rect = collides(rects, e.offsetX, e.offsetY)
         if (rect){
-            console.log('collision ' + rect.x + '/' + rect.y)
+            console.log(rects.indexOf(rect))
         } else {
             console.log('no collision')
         }
