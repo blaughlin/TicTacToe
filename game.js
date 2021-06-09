af4e949702f0b335637a178194f35398699d7363
Bernard Laughlin <bernardlaughlin@gmail.com>
2021-06-09 10:11:09 -0800

    added ablitiy tocreate board array

--- game.js
+++ game.js
@@ -40,3 +40,71 @@ if (canvas && canvas.getContext){
         }
     }, false)
 }
+
+const X = 'X'
+const O = 'O'
+
+let moves = 0
+
+function initial_state() {
+    // Returns starting state of board
+    return [['EMPTY', 'EMPTY', 'EMPTY'],
+            ['EMPTY', 'EMPTY', 'EMPTY'],
+            ['EMPTY', 'EMPTY', 'EMPTY']]
+}
+
+const myBoard = [['O', 'O', 'O'],
+                 ['O', 'X', 'O'],
+                 ['X', 'X', 'X']]
+
+function player(board) {
+    // returns player who has the next turn on a board
+    const x_count = board.reduce(function(a,b) { return a.concat(b) }).filter(player => player === X).length 
+    const o_count = board.reduce(function(a,b) { return a.concat(b) }).filter(player => player === O).length 
+
+    if (x_count === 0 && o_count === 0){
+        return X
+    }
+    else if (x_count > o_count) {
+        return O
+    } else {
+        return X
+    }
+}
+
+function actions(board) {
+    // returns ser of all possible actions (i, j) available on the board
+    const moves = []
+    for (let i=0; i < 3; i++){
+        for (let j=0; j < 3; j++){
+            if (board[i][j] == 'EMPTY'){
+                moves.push([i,j])
+            }
+        }
+    }
+    return moves
+}
+
+function result(board, action) {
+    // returns the board that results from making move (i, j) on the board
+    const newBoard = [...board]
+    newBoard[action[0]][action[1]] = player(board)
+    return newBoard
+}
+
+function resultOppositePlayer(board, action) {
+    // returns the board that results from making move (i, j) on the board
+    if (player(board) === X){
+        let current_player = O
+    } else {
+        current_player = X
+    }
+    const newBoard = [...board]
+    newBoard[action[0]][action[1]] = current_player
+    return newBoard
+}
+
+const emptyBoard = initial_state()
+console.log(emptyBoard)
+let newBoard = result(emptyBoard, [0,0])
+console.log(newBoard)
